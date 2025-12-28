fa9c1d0e903a6a739a42c1020d7fed7ea49476cc
lamz <lamatond444@gmail.com>
2025-12-28 14:41:07 +0800

    initial commit

---
+++ practice/ch1/chapterOne.go
@@ -0,0 +1,5 @@
+package ch1
+
+func TestFunc() string {
+	return "hello world"
+}
---
+++ practice/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"practice/ch1"
+)
+
+func main() {
+
+	fmt.Println(ch1.TestFunc())
+}
